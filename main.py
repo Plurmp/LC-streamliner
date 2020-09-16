604ecfacce8164ab97ccd1fc29abc4ee2702e6b7
plurmp <plurmpmcflurnt@gmail.com>
2020-09-16 11:56:23 -0500

    added author search toggle

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import discord
 
 TOKEN = cred['DISCORD_TOKEN']
 client = discord.Client()
-
+author_search = True
 
 @client.event
 async def on_ready():
@@ -14,15 +14,26 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global author_search
+
     if message.author.id == client.user.id:
         return
 
     if re.findall('^l$', message.content):
         await message.channel.send('sauce lc 3#1')
 
-    if message.author == '661826254215053324' and re.findall("Looking up .+? by .+?\.", message.content):
-        # ID is for License checker bot
-        author = re.search("Looking up .+? by (.+?)\.", message.content).group(1)
-        await message.channel.send(f"sauce -qa {author}")
+    if re.findall('^l asearch .+?$', message.content):
+        as_toggle = re.search('^l asearch (.+?)$', message.content).group(1)
+        if as_toggle == 'on':
+            author_search = True
+        elif as_toggle == 'off':
+            author_search = False
+
+    if message.author == '661826254215053324' \
+            and re.findall('Looking up .+? by .+?\.', message.content) \
+            and author_search:
+        # author ID is for License Checker
+        author = re.search('Looking up .+? by (.+?)\.', message.content).group(1)
+        await message.channel.send(f'sauce -qa {author}')
 
 client.run(TOKEN)
