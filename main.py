8580b40fc5296012abbd6ea841c11a56e9234c4b
plurmp <plurmpmcflurnt@gmail.com>
2020-09-18 11:07:39 -0500

    react to lc embed

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import discord
 TOKEN = cred['DISCORD_TOKEN']
 client = discord.Client()
 author_search = True
+last_sriracha_message = {}
 
 print('In main.py')
 
@@ -19,23 +20,38 @@ async def on_ready():
 @client.event
 async def on_message(message):
     global author_search
+    global last_sriracha_message
 
     if message.author.id == client.user.id:
+        return
+    elif message.author.id == 607661949194469376:  # sriracha
+        last_sriracha_message[message.channel.name] = message
+        return
+
+    if re.findall('^en$', message.content):
+        await last_sriracha_message[message.channel.name].add_reaction('🇺🇸')
+        return
+    elif re.findall('^jp$', message.content):
+        await last_sriracha_message[message.channel.name].add_reaction('🇯🇵')
         return
 
     if re.findall('^l$', message.content):
         await message.channel.send('sauce lc 3#1')
+        return
 
     if re.findall('^l asearch .+?$', message.content):
         as_toggle = re.match('^l asearch (.+?)$', message.content).groups()[0]
         if as_toggle == 'on':
             author_search = True
             await message.channel.send("Author search on")
+            return
         elif as_toggle == 'off':
             author_search = False
             await message.channel.send("Author search off")
+            return
         else:
             print("Author search switch error")
+            return
 
     if message.author.id == 661826254215053324 \
             and re.findall('^Looking up .+? by .+?\.$', message.content) \
@@ -45,9 +61,11 @@ async def on_message(message):
         author = re.match('^Looking up .+? by (.+?)\.$', message.content).groups()[0]
         if author == ():
             await message.channel.send('Could not get author')
+            return
         else:
             time.sleep(2)
             await message.channel.send(f'sauce -qa {author}')
+            return
 
 
 client.run(TOKEN)
