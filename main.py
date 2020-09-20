d047f4ef687b479ed65181c20d509f3361ef0e36
plurmp <plurmpmcflurnt@gmail.com>
2020-09-20 14:47:20 -0500

    fix on regex

--- main.py
+++ main.py
@@ -89,8 +89,8 @@ async def on_message(message):
             except NameError:
                 await message.channel.send('Retry failed')
             return
-        elif re.findall('^asearch .+?', message.content):
-            as_toggle = re.match('^asearch (.+?)$', message.content).groups()[0]
+        elif re.findall('^asearch .+?', part_2.strip()):
+            as_toggle = re.match('^asearch (.+?)$', part_2.strip()).groups()[0]
             if as_toggle == 'on':
                 author_search = True
                 await message.channel.send("Author search on")
