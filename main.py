34b1f463436e64ffb44c4cd3f8a656f4c7d6655d
plurmp <plurmpmcflurnt@gmail.com>
2020-10-25 07:53:27 -0500

    qc again

--- main.py
+++ main.py
@@ -66,6 +66,12 @@ async def on_message(message):
     elif part_1.lower() == 'jp':
         await last_sriracha_embed[message.channel.name].add_reaction('🇯🇵')
         return
+    elif part_1.lower == 'qc':
+        if not part_2:
+            await message.channel.send('sauce 1#1')
+            return
+        if part_2.strip() == 'move':
+            await message.channel.send('sauce ')
     elif part_1.lower() == 'lc':
         if not part_2:
             await message.channel.send('sauce lc 3#1')
@@ -83,7 +89,7 @@ async def on_message(message):
                 name='QC shortcuts',
                 value='`qc`: equivalent to `sauce 1#1`.\n\n'
                 '`qc move`: equivalent to `sauce move 1#1 2`.\n\n',
-                inline=False
+                inline=True
             )
             embed.add_field(
                 name='LC shortcuts',
@@ -93,7 +99,7 @@ async def on_message(message):
                 '`lc retry`: repeats Sriracha\'s last `.lc` command in the channel. Use if License Checker freezes on a search.\n\n'
                 '`lc help` : this.\n\n'
                 '`[en | jp]`: reacts with 🇺🇸 or 🇯🇵 to the last Sriracha message in the channel.\n\n',
-                inline=False
+                inline=True
             )
             embed.set_author(
                 name='LC streamliner',
@@ -131,12 +137,6 @@ async def on_message(message):
             else:
                 await message.channel.send('On or off my guy')
                 return
-    elif part_1.lower == 'qc':
-        if not part_2:
-            await message.channel.send('sauce 1#1')
-            return
-        if part_2.strip() == 'move':
-            await message.channel.send('sauce ')
 
 
 client.run(TOKEN)
