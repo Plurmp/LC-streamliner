791a961701a3bad9ba94e3b0d830e723b2b92af1
plurmp <plurmpmcflurnt@gmail.com>
2021-08-28 00:01:10 -0500

    simplified delete function

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ async def on_message(message):
             else:
                 await message.channel.send(f'sauce move 1#{list_id} 2')
                 return
-        elif cmd == 'delete' or cmd == 'del' or cmd == 'delet':
+        elif cmd == 'del':
             if not list_id:
                 await message.channel.send('sauce delete 1#1')
                 return
@@ -130,7 +130,7 @@ async def on_message(message):
             else:
                 await message.channel.send(f'sauce move 2#{list_id} 3')
                 return
-        elif cmd == 'delete' or cmd == 'del' or cmd == 'delet':
+        elif cmd == 'del':
             if not list_id:
                 await message.channel.send('sauce delete 2#1')
                 return
@@ -152,7 +152,7 @@ async def on_message(message):
             else:
                 await message.channel.send(f'sauce move 3#{list_id} 4')
                 return
-        elif cmd == 'delete' or cmd == 'del' or cmd == 'delet':
+        elif cmd == 'del':
             if not list_id:
                 await message.channel.send('sauce delete 3#1')
                 return
@@ -188,21 +188,21 @@ async def on_message(message):
                 name='QC shortcuts',
                 value='`qc [id]`: equivalent to `sauce 1#[id]` (defaults to 1#1).\n\n'
                       '`qc move [id]`: equivalent to `sauce move 1#[id] 2` (defaults to 1#1).\n\n'
-                      '`qc del/delete/delet [id]`: equivalent to `sauce delete 1#[id]` (defaults to 1#1).\n\n',
+                      '`qc del [id]`: equivalent to `sauce delete 1#[id]` (defaults to 1#1).\n\n',
                 inline=True
             )
             embed.add_field(
                 name='Sorting shortcuts',
                 value='`st [id]`: equivalent to `sauce 2#[id]` (defaults to 2#1).\n\n'
                       '`st move [id]`: equivalent to `sauce move 2#[id] 3` (defaults to 2#1).\n\n'
-                      '`st del/delete/delet [id]`: equivalent to `sauce delete 2#[id]` (defaults to 2#1).\n\n',
+                      '`st del [id]`: equivalent to `sauce delete 2#[id]` (defaults to 2#1).\n\n',
                 inline=True
             )
             embed.add_field(
                 name='LC shortcuts',
                 value='`lc`: equivalent to `sauce lc 3#[id]` (defaults to 3#1).\n\n'
                       '`lc move`: equivalent to `sauce move 3#[id] 4` (defaults to 3#1).\n\n'
-                      '`lc del/delete/delet [id]`: equivalent to `sauce delete 3#[id]` (defaults to 3#1).\n\n'
+                      '`lc del [id]`: equivalent to `sauce delete 3#[id]` (defaults to 3#1).\n\n'
                       '`lc asearch [on | off]`: turns automatic author search on or off (does `sauce -qa [author]` when License Checker identifies the author).\n\n'
                       '`lc retry`: repeats Sriracha\'s last `.lc` command in the channel. Use if License Checker freezes on a search.\n\n'
                       '`lc help` : this.\n\n'
